41a3faafc57948644e6a8118c774e97a4a416ad2
Diego Reyes <reyesdiego@hotmail.com>
2017-03-02 15:55:02 -0300

    Refactor. Uso Class

--- index.js
+++ index.js
@@ -4,27 +4,29 @@
 
 'use strict';
 
-var mail = function (options) {
-    var email = require("emailjs");
+var email = require("emailjs");
 
-    var conf;
+class EmailJs {
+    constructor (options) {
+        var conf;
 
-    this.from = options.from;
-    this.status = options.status;
-    this.throughBcc = options.throughBcc;
+        this.from = options.from;
+        this.status = options.status;
+        this.throughBcc = options.throughBcc;
 
-    conf = {
-        user: options.user,
-        password: options.password,
-        host: options.host,
-        port: options.port,
-        domain: options.domain,
-        ssl: options.ssl
-    };
+        conf = {
+            user: options.user,
+            password: options.password,
+            host: options.host,
+            port: options.port,
+            domain: options.domain,
+            ssl: options.ssl
+        };
 
-    this.server = email.server.connect(conf);
+        this.server = email.server.connect(conf);
+    }
 
-    this.emailSimpleValidate = function (email) {
+    emailSimpleValidate (email) {
         var response = false,
             reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
@@ -32,9 +34,9 @@ var mail = function (options) {
             response = true;
         }
         return response;
-    };
+    }
 
-    this.emailCaseSensitiveValidate = function (email) {
+    emailCaseSensitiveValidate (email) {
         var reg = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
             response = false;
 
@@ -42,9 +44,9 @@ var mail = function (options) {
             response = true;
         }
         return response;
-    };
+    }
 
-    this.emailFreeValidate = function (email) {
+    emailFreeValidate (email) {
         var reg = /^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)([\w-]+\.)+[\w-]{2,4})?$/,
             response = false;
 
@@ -52,72 +54,60 @@ var mail = function (options) {
             response = true;
         }
         return response;
-    };
+    }
 
-};
-
-mail.prototype = {
-    send : function (to, subject, text, attachment, callback) {
-        var self = this,
-            config = {
+    send (to, subject, text, attachment) {
+        return new Promise((resolve, reject) => {
+            var config = {
                 from: this.from,
                 subject: subject
             },
             tos = [];
 
         if (typeof text === 'object') {
-            if (typeof attachment === 'function') {
-                callback = attachment;
-            }
             config.attachment = text;
             config.text = '';
         } else {
             config.text = text;
-            if (typeof attachment === 'function') {
-                callback = attachment;
-            }
         }
 
-        if (typeof to === 'string') {
+        if (to === undefined) {
+            return reject({status: "ERROR", code: "AGP-0001", data: "La cuenta de email es inválida"});
+        } else if (typeof to === 'string') {
             to = to.trim();
             if (!this.emailSimpleValidate(to)) {
-                return callback({status: "ERROR", code: "AGP-0001", data: "La cuenta de email es inválida"});
+                return reject({status: "ERROR", code: "AGP-0001", data: "La cuenta de email es inválida"});
             } else {
                 tos.push(to);
             }
         } else if (typeof to === 'object') {
-            to.forEach(function (item) {
+            to.forEach(item => {
                 item = item.trim();
-                if (!self.emailSimpleValidate(item)) {
-                    return callback({status: "ERROR", code: "AGP-0001", data: "La cuenta de email es inválida"});
-                }
-            });
-            tos = to;
-        }
-
-        if (this.status === true) {
-            if (this.throughBcc) {
-                config.bcc = tos.join(',');
-            } else {
-                config.to = tos.join(',');
+            if (!this.emailSimpleValidate(item)) {
+                return reject({status: "ERROR", code: "AGP-0001", data: "La cuenta de email es inválida"});
             }
-            this.server.send(config, function (err, message) {
-                if (err) {
-                    if (typeof callback === 'function') {
-                        return callback({status: "ERROR", code: "AGP-0002", data: err.message});
-                    }
-                } else {
-                    if (typeof callback === 'function') {
-                        return callback(null, {status: "OK", data: message});
-                    }
-                }
-            });
-        } else {
-            if (typeof callback === 'function') {
-                return callback({status: "ERROR", code: "AGP-0008", data: 'Envío de email deshabilitado en Config'});
-            }
-        }
+        });
+        tos = to;
     }
-};
 
-module.exports = mail;
+    if (this.status === true) {
+    if (this.throughBcc) {
+    config.bcc = tos.join(',');
+} else {
+    config.to = tos.join(',');
+}
+this.server.send(config, (err, message) => {
+    if (err) {
+    reject({status: "ERROR", code: "AGP-0002", data: err.message});
+} else {
+    resolve({status: "OK", data: message});
+}
+});
+} else {
+    reject({status: "ERROR", code: "AGP-0008", data: 'Envío de email deshabilitado en Config'});
+}
+});
+}
+}
+
+module.exports = EmailJs;
